4721b81a297f7979f90d31b9850d5b2179c85109
Rizal Mohd Nor <joey.aka.rizal@gmail.com>
2020-01-21 12:17:51 +0800

    add support for zones

--- solatJakim.py
+++ solatJakim.py
@@ -11,12 +11,11 @@ example usage for now
 printPrayerTimes()
 print ('\n\n')
 printPrayerTimes('asr')
-
 """
 
-
+default_zone = 'SGR01'
 # defining the api-endpoint  
-JAKIM_API = "https://www.e-solat.gov.my/index.php?r=esolatApi/takwimsolat&period=duration&zone=SGR01"
+JAKIM_API = "https://www.e-solat.gov.my/index.php?r=esolatApi/takwimsolat&period=duration&zone="+default_zone
 
 
 #setting the date for POST data
@@ -35,10 +34,77 @@ pastebin_url = r.text
 solatinfo_dict = json.loads(pastebin_url)
 
 waktuBM = {"imsak":"Imsak","fajr":"Subuh","syuruk":"Syuruk","dhuhr":"Zohor","asr":"Asar","maghrib":"Maghrib","isha":"Isyak"}
+zones = {
+    'JHR01':'JHR01 - Pulau Aur dan Pulau Pemanggil',
+    'JHR02':'JHR02 - Johor Bahru, Kota Tinggi, Mersing',
+    'JHR03':'JHR03 - Kluang, Pontian',
+    'JHR04':'JHR04 - Batu Pahat, Muar, Segamat, Gemas Johor',
+    'KDH01':'KDH01 - Kota Setar, Kubang Pasu, Pokok Sena (Daerah Kecil)',
+    'KDH02':'KDH02 - Kuala Muda, Yan, Pendang',
+    'KDH03':'KDH03 - Padang Terap, Sik',
+    'KDH04':'KDH04 - Baling',
+    'KDH05':'KDH05 - Bandar Baharu, Kulim',
+    'KDH06':'KDH06 - Langkawi',
+    'KDH07':'KDH07 - Puncak Gunung Jerai',
+    'KTN01':'KTN01 - Bachok, Kota Bharu, Machang, Pasir Mas, Pasir Puteh, Tanah Merah, Tumpat, Kuala Krai, Mukim Chiku',
+    'KTN03':'KTN03 - Gua Musang (Daerah Galas Dan Bertam), Jeli',
+    'MLK01':'MLK01 - SELURUH NEGERI MELAKA',
+    'NGS01':'NGS01 - Tampin, Jempol',
+    'NGS02':'NGS02 - Jelebu, Kuala Pilah, Port Dickson, Rembau, Seremban',
+    'PHG01':'PHG01 - Pulau Tioman',
+    'PHG02':'PHG02 - Kuantan, Pekan, Rompin, Muadzam Shah',
+    'PHG03':'PHG03 - Jerantut, Temerloh, Maran, Bera, Chenor, Jengka',
+    'PHG04':'PHG04 - Bentong, Lipis, Raub',
+    'PHG05':'PHG05 - Genting Sempah, Janda Baik, Bukit Tinggi',
+    'PHG06':'PHG06 - Cameron Highlands, Genting Higlands, Bukit Fraser',
+    'PLS01':'PLS01 - Kangar, Padang Besar, Arau',
+    'PNG01':'PNG01 - Seluruh Negeri Pulau Pinang',
+    'PRK01':'PRK01 - Tapah, Slim River, Tanjung Malim',
+    'PRK02':'PRK02 - Kuala Kangsar, Sg. Siput (Daerah Kecil), Ipoh, Batu Gajah, Kampar',
+    'PRK03':'PRK03 - Lenggong, Pengkalan Hulu, Grik',
+    'PRK04':'PRK04 - Temengor, Belum',
+    'PRK05':'PRK05 - Kg Gajah, Teluk Intan, Bagan Datuk, Seri Iskandar, Beruas, Parit, Lumut, Sitiawan, Pulau Pangkor',
+    'PRK06':'PRK06 - Selama, Taiping, Bagan Serai, Parit Buntar',
+    'PRK07':'PRK07 - Bukit Larut',
+    'SBH01':'SBH01 - Bahagian Sandakan (Timur), Bukit Garam, Semawang, Temanggong, Tambisan, Bandar Sandakan, Sukau',
+    'SBH02':'SBH02 - Beluran, Telupid, Pinangah, Terusan, Kuamut, Bahagian Sandakan (Barat)',
+    'SBH03':'SBH03 - Lahad Datu, Silabukan, Kunak, Sahabat, Semporna, Tungku, Bahagian Tawau  (Timur)',
+    'SBH04':'SBH04 - Bandar Tawau, Balong, Merotai, Kalabakan, Bahagian Tawau (Barat)',
+    'SBH05':'SBH05 - Kudat, Kota Marudu, Pitas, Pulau Banggi, Bahagian Kudat',
+    'SBH06':'SBH06 - Gunung Kinabalu',
+    'SBH07':'SBH07 - Kota Kinabalu, Ranau, Kota Belud, Tuaran, Penampang, Papar, Putatan, Bahagian Pantai Barat',
+    'SBH08':'SBH08 - Pensiangan, Keningau, Tambunan, Nabawan, Bahagian Pendalaman (Atas)',
+    'SBH09':'SBH09 - Beaufort, Kuala Penyu, Sipitang, Tenom, Long Pa Sia, Membakut, Weston, Bahagian Pendalaman (Bawah)',
+    'SGR01':'SGR01 - Gombak, Petaling, Sepang, Hulu Langat, Hulu Selangor, S.Alam',
+    'SGR02':'SGR02 - Kuala Selangor, Sabak Bernam',
+    'SGR03':'SGR03 - Klang, Kuala Langat',
+    'SWK01':'SWK01 - Limbang, Lawas, Sundar, Trusan',
+    'SWK02':'SWK02 - Miri, Niah, Bekenu, Sibuti, Marudi',
+    'SWK03':'SWK03 - Pandan, Belaga, Suai, Tatau, Sebauh, Bintulu',
+    'SWK04':'SWK04 - Sibu, Mukah, Dalat, Song, Igan, Oya, Balingian, Kanowit, Kapit',
+    'SWK05':'SWK05 - Sarikei, Matu, Julau, Rajang, Daro, Bintangor, Belawai',
+    'SWK06':'SWK06 - Lubok Antu, Sri Aman, Roban, Debak, Kabong, Lingga, Engkelili, Betong, Spaoh, Pusa, Saratok',
+    'SWK07':'SWK07 - Serian, Simunjan, Samarahan, Sebuyau, Meludam','SWK08':'SWK08 - Kuching, Bau, Lundu, Sematan',
+    'SWK09':'SWK09 - Zon Khas (Kampung Patarikan)',
+    'TRG01':'TRG01 - Kuala Terengganu, Marang, Kuala Nerus',
+    'TRG02':'TRG02 - Besut, Setiu',
+    'TRG03':'TRG03 - Hulu Terengganu',
+    'TRG04':'TRG04 - Dungun, Kemaman',
+    'WLY01':'WLY01 - Kuala Lumpur, Putrajaya',
+    'WLY02':'WLY02 - Labuan'
+}
 
+def setDefaultZone(zone):
+    default_zone = zone
+
+def printZone(zone):
+    setDefaultZone(zone)
+    print(zones[default_zone])
+    
 def printPrayerTimes(waktu='all'):
     if (waktu=='all'):
-        print("Waktu solat bagi " + solatinfo_dict['prayerTime'][0]['hijri'] + "\n")
+        print("Bagi Tahun " + solatinfo_dict['prayerTime'][0]['hijri'] + '. Waktu solat bagi kawasan:')
+        print(zones[default_zone]) 
         print(waktuBM['imsak']+" : " + solatinfo_dict['prayerTime'][0]['imsak'])
         print(waktuBM['fajr']+" : " + solatinfo_dict['prayerTime'][0]['fajr'])
         print(waktuBM['syuruk']+" : " + solatinfo_dict['prayerTime'][0]['syuruk'])
@@ -47,27 +113,41 @@ def printPrayerTimes(waktu='all'):
         print(waktuBM['maghrib']+" : " + solatinfo_dict['prayerTime'][0]['maghrib'])
         print(waktuBM['isha']+" : " + solatinfo_dict['prayerTime'][0]['isha'])      
     elif waktu == 'hijri':
-        print("Waktu solat bagi " + solatinfo_dict['prayerTime'][0]['hijri'] + "\n")
+        print("Tahun Islam " + solatinfo_dict['prayerTime'][0]['hijri'])
     else:
         print(waktuBM[waktu]+" : " + solatinfo_dict['prayerTime'][0][waktu])
         
 def main(argv):
     waktu = 'all';
     try:
-        opts, args = getopt.getopt(argv,"hwd",["waktu="])
+        opts, args = getopt.getopt(argv,"hwdz",["waktu="])
     except getopt.GetoptError:
         print 'solatJakim -w <waktusolat>'
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print 'solatJakim -w <imsak|fajr|syuruk|dhuhr|maghrib|isha>'
+            print 'solatJakim -w <imsak|fajr|syuruk|dhuhr|maghrib|isha> -z zone'
+            print 'supported zones are : ' +str(zones)
             sys.exit()
         elif opt in ("-w", "--waktu"):
-            waktu = argv[1]
-            printPrayerTimes(waktu)
+            if len(args)<=1:
+                waktu = args[0]
+                printPrayerTimes(waktu)
+                exit
+            else:
+                if args[1] == '-z':
+                    if args[2] not in zones:
+                        print ("Invalid Zones, valid zones with -z are :" +str(zones))
+                    else:
+                        setDefaultZone(args[2])
+                        printPrayerTimes(waktu)
+                        exit
         elif opt in ("-d", "--islamicDate"):
             printPrayerTimes('hijri')
-  
+            exit
+        elif opt in ("-z", "--zones"):
+            printZone(args[0])
+            exit
 if __name__== "__main__":
     main(sys.argv[1:])
 
